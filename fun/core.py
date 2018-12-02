b1335cf49a190d90a785ec77eff8483938d69a0e
ch.ke <ch.ke@afusion.com>
2018-12-02 21:57:13 +0800

    google drive pdf crawler

---
+++ fun/core.py
@@ -0,0 +1,51 @@
+import pathlib
+from selenium import webdriver
+from os.path import expanduser as ep
+
+
+def firefox():
+    firefox_driver = ep('~') + '/Users/ch.ke/GitHub/taaipdf/geckodriver'
+
+    # create data download folder
+    pathlib.Path('/Users/ch.ke/Desktop/courses/taai2018').mkdir(parents=True, exist_ok=True)
+
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("browser.privatebrowsing.autostart", True)
+    profile.set_preference("browser.download.folderList", 2)
+    profile.set_preference("browser.download.manager.showWhenStarting", False)
+    # download location
+    profile.set_preference("browser.download.useDownloadDir", True)
+    profile.set_preference("browser.download.dir", '/Users/ch.ke/Desktop/courses/taai2018')
+    ftypes = ("application/download,application/x-7z-compressed,text/html,text/plain,application/pdf,image/bmp,"
+              "application/x-bzip,application/x-csh,text/x-c,text/css,text/csv,image/gif,video/h261,"
+              "image/x-icon,text/x-java-source,java,application/javascript,application/json,"
+              "image/jpeg,image/pjpeg,application/x-latex,application/vnd.ms-excel,"
+              "application/vnd.ms-excel.addin.macroenabled.12,application/vnd.ms-excel.sheet.binary.macroenabled.12,"
+              "application/vnd.ms-excel.template.macroenabled.12,application/vnd.ms-excel.sheet.macroenabled.12,"
+              "application/vnd.openxmlformats-officedocument.presentationml.presentation,"
+              "application/vnd.openxmlformats-officedocument.presentationml.slide,"
+              "application/vnd.openxmlformats-officedocument.presentationml.slideshow,"
+              "application/vnd.openxmlformats-officedocument.presentationml.template,"
+              "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,"
+              "application/vnd.openxmlformats-officedocument.spreadsheetml.template,"
+              "application/vnd.openxmlformats-officedocument.wordprocessingml.document,"
+              "application/vnd.openxmlformats-officedocument.wordprocessingml.template,"
+              "application/vnd.ms-powerpoint,application/vnd.ms-powerpoint.addin.macroenabled.12,"
+              "application/vnd.ms-powerpoint.slide.macroenabled.12,"
+              "application/vnd.ms-powerpoint.presentation.macroenabled.12,"
+              "application/vnd.ms-powerpoint.slideshow.macroenabled.12,"
+              "application/vnd.ms-powerpoint.template.macroenabled.12,"
+              "application/msword,application/vnd.ms-word.document.macroenabled.12,"
+              "application/vnd.ms-word.template.macroenabled.12,application/x-mswrite,application/vnd.ms-xpsdocument,"
+              "video/mj2,audio/mpeg,video/mpeg,application/mp21,audio/mp4,video/mp4,application/mp4,"
+              "audio/ogg,video/ogg,application/vnd.oasis.opendocument.image,"
+              "application/vnd.oasis.opendocument.presentation,application/vnd.oasis.opendocument.spreadsheet,"
+              "application/vnd.oasis.opendocument.text,image/png,image/x-citrix-png,image/x-png,video/quicktime,"
+              "text/richtext,image/svg+xml,image/x-rgb,application/zip,application/octet-stream")
+    profile.set_preference("browser.helperApps.neverAsk.saveToDisk", ftypes)
+    profile.set_preference("pdfjs.disabled", "true")
+    profile.update_preferences()
+    driver = webdriver.Firefox(executable_path=firefox_driver, firefox_profile=profile)
+    driver.set_window_size(1390, 850)
+
+    return driver
---
+++ taai.py
@@ -0,0 +1,104 @@
+
+from fun import core
+import time
+from selenium.common.exceptions import StaleElementReferenceException, WebDriverException
+import requests
+
+driver = core.firefox()
+
+
+def download(url, name):
+    response = requests.get(url)
+    pdf = response.content
+    cname = '/Users/ch.ke/Desktop/courses/taai2018/' + name + '.pdf'
+    with open(cname, 'wb') as f:
+        f.write(pdf)
+
+
+def pdfc(d):
+    d.get('http://taai2018.asia.edu.tw/papers/?fbclid=IwAR2eNq-4CWfXD4Q_rF-SuSoXKNbeciCDFq3ZQNOFaS4NTkjYs'
+          'BFxIAu5Z48#!/toc/0')
+
+    j = 0
+    while j < 1000:
+        try:
+            # pdf = d.find_elements_by_class_name('pull-right ng-binding')
+            pdf = d.execute_script("return document.getElementsByClassName('pull-right ng-binding')")
+            option = d.execute_script("return document.getElementsByClassName('pull-right ng-binding').length")
+
+            print('Number of pdfs:', option)
+            if int(option) == 90:
+                break
+            else:
+                j += 1
+                continue
+        except (StaleElementReferenceException, WebDriverException):
+            time.sleep(0.1)
+            j += 1
+            continue
+
+    page = []
+    nopdf = []
+    for i in range(0, len(pdf)):
+        j = 0
+        while j < 1000:
+            try:
+                pgn = d.execute_script("return document.getElementsByClassName('pull-right ng-binding')[" +
+                                       str(i) + "].textContent")
+                pdf[i].click()
+                e = 0
+                while True:
+                    try:
+                        parent = d.window_handles[0]
+                        popup = d.window_handles[1]
+                        break
+                    except IndexError:
+                        time.sleep(0.1)
+                        e += 1
+                        if e < 50:
+                            continue
+                        else:
+                            print('PDF not found after', str(e * 0.1), 'secs')
+                            popup = 'no'
+                            nopdf.append(popup)
+                            break
+                if popup == 'no':
+                    break
+                if parent != popup:
+                    d.switch_to_window(popup)
+                    url = 'about:blank'
+                    while url == 'about:blank':
+                        url = driver.current_url
+                        time.sleep(0.1)
+                    doc_id = url.split("/")[-2]
+                    print(doc_id)
+                    durl = "https://drive.google.com/uc?id={0}&export=download".format(doc_id)
+                    print(durl)
+                    print(pgn)
+                    page.append(pgn)
+                    print('clicking:', i)
+                    # last pdf has direct link
+                    if int(i) == 89:
+                        download(url, str('375(' + pgn + ')'))
+                    else:
+                        download(durl, pgn)
+                    d.close()
+                d.switch_to.window(parent)
+                break
+            except (StaleElementReferenceException, WebDriverException):
+                time.sleep(0.1)
+                j += 1
+                continue
+
+    print('Number of articles with pdf files:', str(len(pdf) - len(nopdf)))
+    return page
+
+
+# for program timing
+start_time = time.time()
+
+pdf = pdfc(driver)
+
+# print time crawler took
+print("Crawler ran for: {0} seconds ({1} mins)".format(str(round(time.time() - start_time, 2)),
+                                                       str(round((time.time() - start_time)/60))))
